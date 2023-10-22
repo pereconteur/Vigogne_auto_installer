83e14649689885f44eda496e51728932f3414e12
PereConteur <114245846+pereconteur@users.noreply.github.com>
2023-10-22 14:57:37 +0200

    Update vigogne_V2_auto_install_Mac_Linux.py
    Précision des questions

--- vigogne_V2_auto_install_Mac_Linux.py
+++ vigogne_V2_auto_install_Mac_Linux.py
@@ -139,7 +139,7 @@ def goForVigogneInstallation(modele_choice, poids_choice, chat_or_instruct):
 
     while True:
         choix = input("\nAvez-vous déja installer le repo de Vigogne sur GitHub "
-                      "ou voulez-vous que je m'en charge ? :\n1 = Oui\n2 = Non\n")
+                      "ou voulez-vous que je m'en charge ? :\n1 = Oui télécharge-le \n2 = Non pas besoin\n")
         if choix == "1":
             echo_yellow("Clonage du dépôt GitHub de Vigogne")
             subprocess.run(["git", "clone", "https://github.com/bofenghuang/vigogne.git"])
@@ -154,7 +154,7 @@ def goForVigogneInstallation(modele_choice, poids_choice, chat_or_instruct):
 
     while True:
         choix = input("\nAvez-vous déja installer les dépendances "
-                      "ou voulez-vous que je m'en charge ? :\n1 = Oui\n2 = Non\n")
+                      "ou voulez-vous que je m'en charge ? :\n1 = Oui, télécharge-les\n2 = Non pas besoin\n")
         if choix == "1":
             subprocess.run(["pip3.10", "install", "deepspeed"])
             subprocess.run(["pip3.10", "install", "."])
